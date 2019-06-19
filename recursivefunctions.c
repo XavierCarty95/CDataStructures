25d2002471642d16beb0205fea0f9cc5c606e6a3
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-06-19 13:59:04 -0400

    recursive functions

---
+++ recursivefunctions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int exitstatus;
+    printf("Everyone should learn coding \n");
+    printf("Enter 1 to continue and 0 to exit ");
+    scanf("%d", &exitstatus);
+    if (exitstatus==1)
+    main(); // a recursive call
+};
