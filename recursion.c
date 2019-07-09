29e3946d32bc903c77669f9ba293428aa22e7beb
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-07-09 17:06:34 -0400

    " basic example of recursion"

---
+++ recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+void print(int n){
+    if(n <= 0){
+    return; 
+    }
+    printf("Hello!\n");
+    print(n-1);
+   
+}
+
+
+
+
+int main(){
+    print(5);
+    return 0;
+}
