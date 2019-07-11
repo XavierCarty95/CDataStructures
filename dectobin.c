90c05edc70c98367a9577278ccfbbde98cafb56b
Xavier Carty <xaviercarty.student@careerdevs.com>
2019-07-11 16:22:55 -0400

    "decimal to binary recursively"

---
+++ dectobin.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void decToBin(unsigned n){
+    if (n == 0){
+        printf("0");
+        return;
+    }
+    else if(n==1){
+        printf("1");
+        return;
+    } else {
+
+    decToBin(n/2);
+    printf("%d", n%2);
+    
+}
+
+}
+int main() {
+    decToBin(25);
+    return 0;
+}
